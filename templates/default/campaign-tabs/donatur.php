0757693c8277b7bf40561a27f0edbf0c07bd2c6a
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-04-02 21:16:04 +0700

    update campaign tabs

--- templates/default/campaign-tabs/donatur.php
+++ templates/default/campaign-tabs/donatur.php
@@ -2,11 +2,8 @@
 <center><i>Tidak ada data!</i></center>
 <?php endif ?>
 <?php foreach($campaign->transactions as $transaction): ?>
-<div class="row">
-    <div class="col col-md-3">
-        
-    </div>
-    <div class="col col-md-9">
+<div class="row mt-3 mx-auto rounded bg-white shadow-sm">
+    <div class="col p-4">
         <span><?=$transaction->created_at?></span>
         <h2>Rp. <?=number_format($transaction->amount)?></h2>
         <h3 class="primary-color"><?= $transaction->subject->is_anonim ? 'Hamba Allah' : $transaction->subject->name ?></h3>
