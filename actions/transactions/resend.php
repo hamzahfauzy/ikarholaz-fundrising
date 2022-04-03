0160f63ecf9c701404676a99825bd4f9e3434075
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-04-04 02:43:36 +0700

    update resend notif

--- actions/transactions/resend.php
+++ actions/transactions/resend.php
@@ -16,23 +16,25 @@ $data->data = $db->single($data->destination_type,[
     'id' => $data->destination_id
 ]);
 
-$detail_url = routeTo('default/transaction-detail',['id'=>$data->id],true);
-$message = '*IKARHOLAZ - GALANG DANA*
+$detail_url = routeTo('default/transaction-detail',['id'=>$data->id,'type'=>'download'],true);
+$message = '*IKARHOLAZ - FUNDRAISING*
 -Notifikasi Pembayaran kode *#'.$data->pg->Data->TransactionId.'* Berhasil-
 
-Yth, '.$data->subject->name.'
+Hai kak '.$data->subject->name.'
 
 Terima kasih sudah berpartisipasi untuk program *"'.$data->data->name.'"* pada tanggal pada tanggal *'.$data->created_at.'* dengan menggunakan metode pembayaran *'.$data->pg->Data->Channel.' - '.$data->pg->Data->Via.'*.  TELAH BERHASIL.
 
-_Total Pembayaran_ *'.number_format($data->amount).'*
+Pembayaran sejumlah *Rp. '.number_format($data->amount).'* telah kami terima. Klik '.$detail_url.' untuk mengunduh tanda terima pembayaran.
 
-Semoga kebaikan Sdr/i '.$data->subject->name.' diberikan balasan yang berlipat ganda.
+Semoga kebaikan Kak '.$data->subject->name.' diberikan balasan yang berlipat ganda.
 Amin.
 
-Hormat kami,
-*IKARHOLAZ*
+Terima kasih,
 
-Terlampir kami kirim bukti pembayaran format PDF atau klik '.$detail_url.' untuk mengunduhnya. Jika ada pertanyaan silakan hubungi kami langsung di inbox@ikarholaz.com atau di +62 838-0661-1212';
+_Jika ada pertanyaan silakan hubungi langsung di inbox@ikarholaz.com atau di +62 838-0661-1212_
+
+*IKARHOLAZ FUNDRAISING*
+_part of Sistem Informasi Rholaz (SIR) © 2022_';
 
 $wablast = WaBlast::send($data->subject->phone, $message);
 
