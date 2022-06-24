106c6f419addcff9b9931dd41468ec322b475984
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-06-24 17:15:28 +0700

    add testing wa api

--- actions/api/test.php
+++ actions/api/test.php
@@ -1,5 +1,12 @@
 <?php
 
+$message = 'this is testing from website';
+
+$wablast = WaBlast::send("082369378823", $message);
+
 echo json_encode([
-    'msg' => 'hello world'
+    'msg' => 'hello world',
+    'result' => $wablast
 ]);
+
+die();
